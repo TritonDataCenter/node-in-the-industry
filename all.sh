5655f6cdb6da613f2ef63fc26897bf33a65b7ab2
isaacs <i@izs.me>
2013-05-09 15:50:23 -0700

    An index page that lists them all

---
+++ all.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/bash
+
+set -e
+
+# http://mywiki.wooledge.org/BashFAQ/026
+shuffle() {
+  local i tmp size max rand
+
+  # $RANDOM % (i+1) is biased because of the limited range of $RANDOM
+  # Compensate by using a range which is a multiple of the array size.
+  size=${#array}
+  max=$(( 32768 / size * size ))
+
+  for ((i=size-1; i>0; i--)); do
+    while (( (rand=$RANDOM) >= max )); do :; done
+    rand=$(( rand % (i+1) ))
+    tmp=${array[i]} array[i]=${array[rand]} array[rand]=$tmp
+  done
+}
+
+array=($(echo data/*))
+shuffle
+
+for company in "${array[@]}"; do
+  name=${company#data/}
+  img=$company/logo.png
+  quote=$company/quote.html
+  cat <<END
+  <div class="row clearfix">
+    <p><img src="$img" alt="$name"></p>
+END
+  cat $quote
+  cat <<END
+  </div>
+END
+done
---
+++ make-index.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+(cat head.html; bash all.sh; cat foot.html) > index.html
